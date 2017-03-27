c543b23765462ff776aa11b2edc8352250be5187
lbernasl <miguelbernardinoferreira@gmail.com>
2017-03-27 11:12:21 +0100

    aaa

--- platforms/android/src/main/assets/app/views/formcreate/create.js
+++ platforms/android/src/main/assets/app/views/formcreate/create.js
@@ -1,12 +1,22 @@
+var http = require("http");
 var layout = require("ui/layouts/stack-layout");
 var buttonModule = require("ui/button");
 var frameModule = require("ui/frame");
 var labelModule = require("ui/label");
+var textfieldModule = require("ui/text-field");
 var page;
 
 exports.loadCreate = function(args) {
     console.info("ESTÁ A FUNCIONAR CARALHO.");
     page = args.object;
+    var size = 5;
 
+    var newStackLayout = new layout.StackLayout();
+    var labelTextBox = new labelModule.Label;
+    var textfieldTextBox = new textfieldModule.TextField;
+
+    newStackLayout.addChild(labelTextBox);
+    newStackLayout.addChild(textfieldTextBox);
     
+    page.content = newStackLayout;
 }
