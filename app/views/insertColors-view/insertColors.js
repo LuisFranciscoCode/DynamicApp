0156c31785a30f7ecb804aed7efa90611d6d8fa8
LuisFranciscoCode <luisfranciscocode@gmail.com>
2017-03-27 17:07:59 +0100

    minUpdate inserir cores

--- app/views/insertColors-view/insertColors.js
+++ app/views/insertColors-view/insertColors.js
@@ -2,6 +2,7 @@ var layout = require("ui/layouts/stack-layout");
 var buttonModule = require("ui/button");
 var textfieldModule = require("ui/text-field");
 var utilityModule = require("utils/utils");
+var http = require("http");
 
 exports.inserirCores = function(args) {
     var stackLayout = new layout.StackLayout();
@@ -31,7 +32,16 @@ drawButton = function(sLayout) {
     button.on(buttonModule.Button.tapEvent, function() {
         console.info(textfield1.text);
         console.info(textfield2.text);
-        utilityModule.openUrl("https://luisfranciscocode.000webhostapp.com/servidor.php?teste1=" + textfield1.text + "&&teste2=" + textfield2.text);
+
+        http.request({ url: "https://luisfranciscocode.000webhostapp.com/servidor.php?teste1=" + textfield1.text + "&&teste2=" + textfield2.text, method: "GET" }).then(function (response) {
+            alert("You just inserted a new color");
+            textfield1.text = "";
+            textfield2.text = "";
+        }, function (e) {
+           console.info(e);
+        });
+
+
     });
 
     sLayout.addChild(button);
