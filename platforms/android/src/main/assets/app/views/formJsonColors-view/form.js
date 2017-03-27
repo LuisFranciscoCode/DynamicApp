df84907ee9a72819d27e7ea293b6126db15376ea
LuisFranciscoCode <luisfranciscocode@gmail.com>
2017-03-27 10:38:08 +0100

    testeAquiHugo

--- platforms/android/src/main/assets/app/views/formJsonColors-view/form.js
+++ platforms/android/src/main/assets/app/views/formJsonColors-view/form.js
@@ -1,20 +1,31 @@
 var layout = require("ui/layouts/stack-layout");
 var buttonModule = require("ui/button");
 var labelModule = require("ui/label");
+var observable = require("data/observable");
+var observableArrayModule = require("data/observable-array");
+var arrayTesteObervable;
+
+var Botao = {
+    NomeBotao: "",
+    CodigoBotao: ""
+}
 
 exports.formulario = function(args) {
     page = args.object;
+    
     readJson();
+
 }
 
 readJson = function() {
-    fetch("http://10.0.7.102/teste/coresV2.json").then(response => { 
+    // fetch("http://10.0.7.102/teste/coresV2.json").then(response => { 
+    fetch("https://api.myjson.com/bins/16xfab").then(response => { 
         return response.json();
      })
      .then(function (r) {
-        
+        console.info("json chegou");    
         var dataJson = r;
-        var numDataJson = Object.keys(dataJson.name).length;
+        var numDataJson = Object.keys(dataJson.Name).length;
 
         console.info(numDataJson);
         drawButtons(dataJson, numDataJson);
@@ -24,34 +35,53 @@ readJson = function() {
 drawButtons = function(values, totalValues) {
     var newStackLayout = new layout.StackLayout();
 
-     var saveColor = new Array(totalValues);
+    var saveColor = new Array(totalValues);
+     
 
     for(i = 0; i <= totalValues; i++) {
         var arrayButton = new Array(i);
-        var umacoisaqualquer = values.info[i].code;
-
-        arrayButton[i] = new buttonModule.Button();
-        arrayButton[i].text = values.info[i].name;
-        arrayButton[i].value = umacoisaqualquer[i];
         
-        arrayButton[i].on(buttonModule.Button.tapEvent, function() {       
-            // arranjar maneira de mudar cor conforme o botão carregado
-            // arranjar maneira de mudar cor conforme o botão carregado
-            // arranjar maneira de mudar cor conforme o botão carregado
-            // arranjar maneira de mudar cor conforme o botão carregado
-        });       
+       var btn = new observableArrayModule.ObservableArray(Botao);
 
-        console.info(values.info[i].name);
-        console.info(values.info[i].code);
+        saveColor[i] = values.info[i].Code;
+        arrayButton[i] = new buttonModule.Button();
+        arrayButton[i].text = values.info[i].Name;
+        // arrayButton[i].value = values.info[i].Name;
 
-        newStackLayout.addChild(arrayButton[i]);
-    }
+        btn.set("NomeBotao", values.info[i].Name);
+        btn.set("CodigoBotao", values.info[i].Code);
 
+        arrayButton[i].on(buttonModule.Button.tapEvent, function(args) {       
+            console.info("kkkkkkkk");
+            console.info("Property Changed!");
+            console.info("Text: " + args.text);
+            console.info("Event name:" + args.eventName);
+            console.info("Object:" + args.object);
+            console.info("propertyname:" + args.propertyName);
+            console.info("value:" + args.value); 
+        });
+        
+        arrayTesteObervable = new observableArrayModule.ObservableArray(arrayButton);
+        console.dump(btn);
+        newStackLayout.addChild(arrayButton[i]);  
+    } 
+      
+    
+    
+    // funcaoImportante(arrayTesteObervable);
     page.content = newStackLayout;
+}
+
+funcaoImportante = function(arrayColors) {
+    num = arrayColors.length;
+    console.info("Numero: " + num);
+  
+    for (i = 0; i < num; i++) {
+        console.info("GetItem(" + i + "): " + arrayColors.getItem(i));
+    } 
 }
 
 changeColor = function(color) {
     newStackLayout.backgroundColor = color;
     page.content = newStackLayout;
 }
-
