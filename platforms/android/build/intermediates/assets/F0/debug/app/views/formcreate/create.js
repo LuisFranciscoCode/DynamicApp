29bbf76ddaa12f2569aef1bee8fd8cad15c6eaa9
lbernasl <miguelbernardinoferreira@gmail.com>
2017-03-28 14:17:05 +0100

    faka u gradlew

--- platforms/android/build/intermediates/assets/F0/debug/app/views/formcreate/create.js
+++ platforms/android/build/intermediates/assets/F0/debug/app/views/formcreate/create.js
@@ -6,77 +6,92 @@ var labelModule = require("ui/label");
 var textfieldModule = require("ui/text-field");
 var page;
 var topmost = frameModule.topmost();
+
 var submitButton = new buttonModule.Button();
     submitButton.name = "submitButton";
     submitButton.text = "Ok";
-    
+
+
 exports.loadCreate = function(args) {
     console.info("ESTÁ A FUNCIONAR CARALHO.");
     page = args.object;
-    var size = 5;
-    var newStackLayout = new layout.StackLayout();
+    if(page.content == null) {
+        var newStackLayout = new layout.StackLayout();
+        var labelTextBox = new labelModule.Label();
+        var textfieldTextBox = new textfieldModule.TextField();
+        labelTextBox.name = "labelTextBox";
+        labelTextBox.text = "Quantas TextBoxes quer?";
+        textfieldTextBox.name = "textfieldTextBox";
+        textfieldTextBox.id = "textfieldTextBox";
+        textfieldTextBox.hint = "Insira um número";
+        textfieldTextBox.keyboardType = "number";
 
-    var labelTextBox = new labelModule.Label();
-    var textfieldTextBox = new textfieldModule.TextField();
-    labelTextBox.name = "labelTextBox";
-    labelTextBox.text = "Quantas TextBoxes quer?";
-    textfieldTextBox.name = "textfieldTextBox";
-    textfieldTextBox.hint = "Insira um número";
+        var labelCheckBox = new labelModule.Label();
+        var textfieldCheckBox = new textfieldModule.TextField();
+        labelCheckBox.name = "labelCheckBox";
+        labelCheckBox.text = "Quantas CheckBoxes quer?";
+        textfieldCheckBox.name = "textfieldCheckBox";
+        textfieldCheckBox.id = "textfieldCheckBox";
+        textfieldCheckBox.hint = "Insira um número";
+        textfieldCheckBox.keyboardType = "number";
 
-    var labelCheckBox = new labelModule.Label();
-    var textfieldCheckBox = new textfieldModule.TextField();
-    labelCheckBox.name = "labelCheckBox";
-    labelCheckBox.text = "Quantas CheckBoxes quer?";
-    textfieldCheckBox.name = "textfieldCheckBox";
-    textfieldCheckBox.hint = "Insira um número";
+        var labelComboBox = new labelModule.Label();
+        var textfieldComboBox = new textfieldModule.TextField();
+        labelComboBox.name = "labelComboBox";
+        labelComboBox.text = "Quantas ComboBoxes quer?";
+        textfieldComboBox.name = "textfieldComboBox";
+        textfieldComboBox.id = "textfieldComboBox";
+        textfieldComboBox.hint = "Insira um número";
+        textfieldComboBox.keyboardType = "number";
 
-    var labelComboBox = new labelModule.Label();
-    var textfieldComboBox = new textfieldModule.TextField();
-    labelComboBox.name = "labelComboBox";
-    labelComboBox.text = "Quantas ComboBoxes quer?";
-    textfieldComboBox.name = "textfieldComboBox";
-    textfieldComboBox.hint = "Insira um número";
+        var labelRadio = new labelModule.Label();
+        var textfieldRadio = new textfieldModule.TextField();
+        labelRadio.name = "labelRadio";
+        labelRadio.text = "Quantos Radio Buttons quer?";
+        textfieldRadio.name = "textfieldRadio";
+        textfieldRadio.id = "textfieldRadio";
+        textfieldRadio.hint = "Insira um número";
+        textfieldRadio.keyboardType = "number";
 
-    var labelRadio = new labelModule.Label();
-    var textfieldRadio = new textfieldModule.TextField();
-    labelRadio.name = "labelRadio";
-    labelRadio.text = "Quantos Radio Buttons quer?";
-    textfieldRadio.name = "textfieldRadio";
-    textfieldRadio.hint = "Insira um número";
+        var labelDatePicker = new labelModule.Label();
+        var textfieldDatePicker = new textfieldModule.TextField();
+        labelDatePicker.name = "labelDatePicker";
+        labelDatePicker.text = "Quantos DatePickers quer?";
+        textfieldDatePicker.name = "textfieldDatePicker";
+        textfieldDatePicker.id = "textfieldDatePicker";
+        textfieldDatePicker.hint = "Insira um número";
+        textfieldDatePicker.keyboardType = "number";
 
-    var labelDatePicker = new labelModule.Label();
-    var textfieldDatePicker = new textfieldModule.TextField();
-    labelDatePicker.name = "labelDatePicker";
-    labelDatePicker.text = "Quantos DatePickers quer?";
-    textfieldDatePicker.name = "textfieldDatePicker";
-    textfieldDatePicker.hint = "Insira um número";
+        submitButton.on(buttonModule.Button.tapEvent, function() {
+            var navigationOptions={
+            moduleName: 'views/form-view/form',
+            context:{textboxes: page.getViewById("textfieldTextBox").text,
+                    checkboxes: page.getViewById("textfieldCheckBox").text,
+                    comboboxes: page.getViewById("textfieldComboBox").text,
+                    radiobuttons: page.getViewById("textfieldRadio").text,
+                    datepicks: page.getViewById("textfieldDatePicker").text
+                }}
+                
+            topmost.navigate(navigationOptions);
+        });
 
-    submitButton.on(buttonModule.Button.tapEvent, function() {
-         var navigationOptions={
-        moduleName: 'views/form-view/form',
-        context:{param1: "value1",
-                param2: "value2"
-            }}
-            
-        topmost.navigate(navigationOptions); 
-    });
+        newStackLayout.addChild(labelTextBox);
+        newStackLayout.addChild(textfieldTextBox);
 
-    newStackLayout.addChild(labelTextBox);
-    newStackLayout.addChild(textfieldTextBox);
+        newStackLayout.addChild(labelCheckBox);
+        newStackLayout.addChild(textfieldCheckBox);
 
-    newStackLayout.addChild(labelCheckBox);
-    newStackLayout.addChild(textfieldCheckBox);
+        newStackLayout.addChild(labelComboBox);
+        newStackLayout.addChild(textfieldComboBox);
 
-    newStackLayout.addChild(labelComboBox);
-    newStackLayout.addChild(textfieldComboBox);
+        newStackLayout.addChild(labelRadio);
+        newStackLayout.addChild(textfieldRadio);
 
-    newStackLayout.addChild(labelRadio);
-    newStackLayout.addChild(textfieldRadio);
+        newStackLayout.addChild(labelDatePicker);
+        newStackLayout.addChild(textfieldDatePicker);
 
-    newStackLayout.addChild(labelDatePicker);
-    newStackLayout.addChild(textfieldDatePicker);
+        newStackLayout.addChild(submitButton);
 
-    newStackLayout.addChild(submitButton);
-
-    page.content = newStackLayout;
+        page.content = newStackLayout;
+    }
 }
