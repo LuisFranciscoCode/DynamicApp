7e35c109a719ff6879fc604b1d61257cb7620880
lbernasl <miguelbernardinoferreira@gmail.com>
2017-03-28 14:55:31 +0100

    agora ai da resete né cara?

--- app/views/form-view/form.js
+++ app/views/form-view/form.js
@@ -1,12 +1,9 @@
-function onNavigatingTo(args) {
-    console.log("oi");
+exports.loadForm = function(agrs){
+console.log("oi");
     var page = args.object;
     
     var gotData=page.navigationContext;
     console.log(gotData.textboxes);
     console.log(gotData.checkboxes);
-}
-exports.loadForm = function(){
-exports.onNavigatingTo = onNavigatingTo;
 
 }
